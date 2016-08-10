f7197465d75ae1e4601a2f3dcea60286775b62dd
Andrew Koh <akscanb@gmail.com>
2016-08-09 20:28:34 -0400

    port reconfig

--- device/app/app.js
+++ device/app/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('.html', require('ejs').renderFile);
